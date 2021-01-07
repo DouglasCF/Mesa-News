80b37280206dafb1fdc7fe8fba83c1ba940dfe67
Douglas Fornaro <douglasfornaro@hotmail.com>
2021-01-07 18:37:14 -0300

    Creating the Create Account screen

--- app/src/main/java/br/com/fornaro/mesanews/features/createaccount/CreateAccountFragment.kt
+++ app/src/main/java/br/com/fornaro/mesanews/features/createaccount/CreateAccountFragment.kt
@@ -5,18 +5,40 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import br.com.fornaro.mesanews.R
+import androidx.navigation.findNavController
+import br.com.fornaro.mesanews.databinding.FragmentCreateAccountBinding
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class CreateAccountFragment : Fragment() {
 
     private val viewModel: CreateAccountViewModel by viewModel()
 
+    private var _binding: FragmentCreateAccountBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_create_account, container, false)
+    ) = FragmentCreateAccountBinding
+        .inflate(inflater)
+        .apply {
+            _binding = this
+        }
+        .root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupBackButton()
+    }
+
+    private fun setupBackButton() = with(binding.backButton) {
+        setOnClickListener { findNavController().navigateUp() }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
